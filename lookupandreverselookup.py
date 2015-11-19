fe2b375ffa65068037571dc6cbb1c5717308722d
ramkumarg1 <ramkumarg1@gmail.com>
2015-11-19 14:31:08 -0800

    Update lookupandreverselookup.py

--- lookupandreverselookup.py
+++ lookupandreverselookup.py
@@ -3,16 +3,17 @@
 #Load  3 digit abbreveations and Country names, use it for looks ups within text,
 #Also reverse lookup the dictiorny so both the formats looks up give positive results
 
+#Data used from http://www.worldatlas.com/aatlas/ctycodes.htm
 
-mydict = {}
+myDict = {}
 
 for lines in open("data/abrcountry.txt"):
         fields = lines.split('\t')
-        mydict[fields[2]]=fields[0].lower()
+        myDict[fields[2]]=fields[0].lower()
 
-revdict = dict(map(reversed, mydict.iteritems()))
+revDict = dict(map(reversed, myDict.iteritems()))
 
-strtoget = list('IND,India,United Kingdom,Australia,AUS,Moved Boxes in'.split(','))
+strToGet = list('IND,India,United Kingdom,Australia,AUS,Moved Boxes in'.split(','))
 
-for value in  map(lambda x: mydict.get(x.upper(), revdict.get(x.lower(),x.lower() + ' : Not Found')),strtoget):
+for value in  map(lambda x: myDict.get(x.upper(), revDict.get(x.lower(),x.lower() + ' : Not Found')),strToGet):
         print str(list(value)[0].upper())+str(''.join(list(value)[1:]))
